f797873051026c5db324f6833eeb3d6df0ec4554
itsnotdevin <itsnotdevin@gmail.com>
2017-03-23 17:10:09 -0400

    added php files.
    1. Removed production values for SQL server login in dbconfig.php
    2. Removed production values for outgoing mail server in class.user.php

---
+++ dbconfig.php
@@ -0,0 +1,28 @@
+<?php
+class Database
+{
+
+    private $host = "localhost";
+    private $db_name = "db_name";
+    private $username = "root_username";
+    private $password = "root_pw";
+    public $conn;
+
+    public function dbConnection()
+ {
+
+     $this->conn = null;
+        try
+  {
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+   $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        }
+  catch(PDOException $exception)
+  {
+            echo "Connection error: " . $exception->getMessage();
+        }
+
+        return $this->conn;
+    }
+}
+?>
